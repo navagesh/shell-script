f9c8659623f231ea57e9f16d71f5ebd4c710c634
navagesh <deekondanagesh@gmail.com>
2025-07-16 19:30:53 +0530

    added 2nd script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/#!/usr/bin/env bash
 
-echo "please enter your username"
+echo "please enter your username::"
 
 read  -s USERNAME  #the value entered above will be automatically attached to USERNAME variable
 
-echo "username is: $USERNAME", password is:$PASSWORD" 
+echo "please enter your password::"
+read -s PASSWORD
+
+echo "username is: $USERNAME", password is:$PASSWORD"
