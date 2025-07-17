6dabad05da0e4dbae345f84ade1bd9b654443990
navagesh <deekondanagesh@gmail.com>
2025-07-17 21:51:14 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -17,3 +17,33 @@ else
 fi
 
 yum install mysql -y
+
+  if [ $? -ne 0 ]
+
+then
+
+  echo "ERROR:: installing mysql is failed"
+
+  exit 1
+
+else
+
+  echo "installing mysql is success"
+
+fi
+
+yum install git -y
+
+  if[ $? -ne 0 ]
+
+then
+
+  echo "ERROR:: Installing GIT is failed"
+
+  exit 1
+
+else
+
+  echo "Installing GIT is success"
+
+fi
