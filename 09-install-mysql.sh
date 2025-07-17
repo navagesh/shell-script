09e6a79d6dbb686bcbf161a677d834444ecec706
navagesh <deekondanagesh@gmail.com>
2025-07-17 21:03:38 +0530

    added 2nd script

---
+++ 09-install-mysql.sh
@@ -0,0 +1,19 @@
+!#/bin/#!/usr/bin/env bash
+
+ID=$(id=u)
+
+  if [$ID -ne 0]
+
+then
+
+  echo "ERROR:please run this script with root access"
+
+  exit 1 #you can give other than 0
+
+else
+
+  echo "you are root user"
+
+fi
+
+yum install mysql -y
