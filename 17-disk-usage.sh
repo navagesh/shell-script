0cd3cfc44daf36da7ae35badc523006f2cdeb8b5
navagesh <deekondanagesh@gmail.com>
2025-07-23 23:02:38 +0530

    new files

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -6,7 +6,7 @@ while IFS= read line
 do
   usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
   partition=$(echo $line | awk '{print $1F}')
-  if [ $usage - ge $DISK_THRESHOLD ]
+  if [ $usage - gt $DISK_THRESHOLD ]
   then
     messages+="High Disk usage on $partition: $usage\n"
   fi
