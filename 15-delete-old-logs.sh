67443af6ab5247076c059cada3943883d15d4c82
navagesh <deekondanagesh@gmail.com>
2025-07-23 21:38:18 +0530

    new files

---
+++ 15-delete-old-logs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash/
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -d $SOURCE_DIR]
+then
+  echo -e "$R source directory: $SOURCE_DIR does not exist $N"
+
+fi
