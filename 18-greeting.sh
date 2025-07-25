31380e0e19ae7bca9750989271466a95aa3827c6
navagesh <deekondanagesh@gmail.com>
2025-07-25 17:31:00 +0530

    new files

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,3 +1,30 @@
 #!/bin/#!/usr/bin/env bash
-person=$1
-echo"Hello $person, good morning.we are learning shell script"
+
+NAME=" "
+WISHES="Good morning"
+USAGE(){
+  echo "USAGE:: $(username $0) -n <name> -w <wishes>"
+  echo "option::"
+  echo "-n,specify the name (mandatory)"
+  echo "-w,specify the wishes, default is good morning"
+  echo "-h,display help and exit"
+
+}
+while getopts ":n:w:h" opt; do
+  case $opt in
+    n) NAME="$OPTARG";;
+    w) WISHES="$OPTARG";;
+    \?) echo "invalid options: -"OPTARG" >&2; USAGE; EXIT;;
+    :) USAGE ; exit;;
+    h) USAGE; exit;;
+  esac
+done
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
+if [ -z "$NAME" ]; then # now wishes is optional
+    #echo "ERROR: Both -n and -w are mandatory options."
+    echo "ERROR: -n is mandatory."
+    USAGE
+    exit 1
+fi
+
+echo "Hello $NAME. $WISHES. I have been learning Shell Script."
