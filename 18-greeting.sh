0f1fecad977f0aec5bf940f34595ba4224100f16
navagesh <deekondanagesh@gmail.com>
2025-07-25 17:38:41 +0530

    new files

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -19,13 +19,11 @@ while getopts ":n:w:h" opt; do
     h) USAGE; exit;;
   esac
 done
-
- #if [ -z "$NAME" ] || [ -z "$WISHES" ]; then
-if [ -z "$NAME" ]; then # now wishes is optional
+  if [ -z "$NAME" ]; then # now wishes is optional
     #echo "ERROR: Both -n and -w are mandatory options."
     echo "ERROR: -n is mandatory."
     USAGE
     exit 1
-fi
+  fi
 
 echo "Hello $NAME. $WISHES. I have been learning Shell Script."
